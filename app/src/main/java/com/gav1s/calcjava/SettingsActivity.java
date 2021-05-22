e5a3722369ca189808d19ca46e1b3e0d226bdab2
GaV1S <GaV1S@yandex.ru>
2021-05-22 10:20:33 +0300

    lesson5homework cleanup code

--- app/src/main/java/com/gav1s/calcjava/SettingsActivity.java
+++ app/src/main/java/com/gav1s/calcjava/SettingsActivity.java
@@ -1,113 +1,85 @@
 package com.gav1s.calcjava;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
-    private static final String appTheme = "APP_THEME";
-    private static final String NameSharedPreference = "THEME";
-
-    private static final int MyCoolCodeStyle = 0;
-    private static final int AppThemeLightCodeStyle = 1;
-    private static final int AppThemeCodeStyle = 2;
-    private static final int AppThemeDarkCodeStyle = 3;
-
-    TypedValue outValue = new TypedValue();
+    Button buttonApply;
+    RadioGroup radioGroup;
+    private int themeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String currentTheme = getIntent().getExtras().getString(MainActivity.YOUR_THEME);
-        switch (currentTheme) {
-            case "dark": {
-                setTheme(getAppTheme(R.style.AppThemeDark));
-            }
-            case "light": {
-                setTheme(getAppTheme(R.style.AppThemeLight));
-            }
-            case "myCool": {
-                setTheme(getAppTheme(R.style.MyCoolStyle));
-            }
-            case "appTheme": {
-                setTheme(getAppTheme(R.style.AppTheme));
-            }
-        }
+        themeId = getIntent().getExtras().getInt(MainActivity.PreferenceKey);
+        doSetTheme(themeId);
         setContentView(R.layout.activity_settings);
-
         initView();
     }
 
-    private void initView() {
-        findViewById(R.id.radioNight)
-                .setOnClickListener(v -> {
-                    setAppTheme(AppThemeDarkCodeStyle);
-                    recreate();
-                });
-        findViewById(R.id.radioLight)
-                .setOnClickListener(v -> {
-                    setAppTheme(AppThemeLightCodeStyle);
-                    recreate();
-                });
-        findViewById(R.id.radioAT)
-                .setOnClickListener(v -> {
-                    setAppTheme(AppThemeCodeStyle);
-                    recreate();
-                });
-        findViewById(R.id.radioCool)
-                .setOnClickListener(v -> {
-                    setAppTheme(MyCoolCodeStyle);
-                    recreate();
-                });
-        Button buttonReturn = findViewById(R.id.btnReturn);
-        buttonReturn.setOnClickListener(v -> {
-            Intent intentResult = new Intent();
-            getTheme().resolveAttribute(R.attr.themeName, outValue, true);
-            intentResult.putExtra(MainActivity.YOUR_THEME, createTheme());
-            setResult(Activity.RESULT_OK, intentResult);
-
-            finish();
-        });
-    }
-
-    private YourTheme createTheme() {
-        getTheme().resolveAttribute(R.attr.themeName, outValue, true);
-        YourTheme yourTheme = new YourTheme(outValue.string.toString());
-        return yourTheme;
-    }
-
-    protected void setAppTheme(int code) {
-        SharedPreferences sharedPreferences = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
-        sharedPreferences.edit()
-                .putInt(appTheme, code)
-                .apply();
-    }
-
-    protected int getAppTheme(int code) {
-        return codeStyleByStyleId(getCodeStyleId(code));
-    }
-
-    protected int codeStyleByStyleId(int code) {
-        switch (code) {
-            case AppThemeCodeStyle:
-                return R.style.AppTheme;
-            case AppThemeLightCodeStyle:
-                return R.style.AppThemeLight;
-            case AppThemeDarkCodeStyle:
-                return R.style.AppThemeDark;
-            case MyCoolCodeStyle:
+    private void doSetTheme(int themeId) {
+        switch (themeId) {
+            case 0:
+                setTheme(R.style.Theme_MaterialComponents_DayNight_DarkActionBar);
+                break;
+            case 1:
+                setTheme(R.style.AppThemeDark);
+                break;
+            case 2:
+                setTheme(R.style.Theme_Design_Light);
+                break;
             default:
-                return R.style.MyCoolStyle;
+                throw new IllegalStateException("Unexpected value: " + themeId);
         }
     }
 
-    protected int getCodeStyleId(int codeStyle) {
-        SharedPreferences sharedPreferences = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
-        return sharedPreferences.getInt(appTheme, codeStyle);
+    private void initView() {
+        radioGroup = findViewById(R.id.radioGroup);
+        buttonApply = findViewById(R.id.btnReturn);
+        initOnClickListeners();
+        selectRadioButton();
+    }
+
+    private void selectRadioButton() {
+        switch (themeId) {
+            case 0:
+                radioGroup.check(R.id.radioNight);
+                break;
+            case 1:
+                radioGroup.check(R.id.radioLight);
+                break;
+            case 2:
+                radioGroup.check(R.id.radioCool);
+                break;
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void initOnClickListeners() {
+        buttonApply.setOnClickListener(v -> {
+            switch (radioGroup.getCheckedRadioButtonId()) {
+                case R.id.radioNight:
+                    themeId = 0;
+                    break;
+                case R.id.radioLight:
+                    themeId = 1;
+                    break;
+                case R.id.radioCool:
+                    themeId = 2;
+                    break;
+            }
+            SharedPreferences pref = getSharedPreferences(MainActivity.PreferenceKey, MODE_PRIVATE);
+            pref.edit().putInt(MainActivity.MyThemeKey, themeId).apply();
+            Intent intentResult = new Intent();
+            setResult(RESULT_OK, intentResult);
+            finish();
+        });
     }
 }
+
